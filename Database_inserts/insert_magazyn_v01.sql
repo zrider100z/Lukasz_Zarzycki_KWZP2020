2f449b3d49c4fe36a237134db40827d44917111b
Lukasz Zarzycki <zrider100z@wp.pl>
2020-04-27 14:59:07 +0200

    Added unit to current warehouse storage retrieving script.

--- Database_inserts/insert_magazyn_v01.sql
+++ Database_inserts/insert_magazyn_v01.sql
@@ -270,9 +270,9 @@ VALUES
 	(4,1,200);
 
 --aktualny stan magazynowy
-SELECT Elementy.ID_Element, Element_Nazwa, Ilosc_paczek_total, Ilosc_paczek_total*Element_Ilosc_W_Paczce FROM (
+SELECT Elementy.ID_Element, Element_Nazwa, Ilosc_paczek_total, Ilosc_paczek_total*Element_Ilosc_W_Paczce, Jednostka FROM (
 SELECT Elementy.ID_Element, SUM( Ilosc_Paczek) AS Ilosc_paczek_total  From Zawartosc INNER JOIN Elementy ON Zawartosc.ID_Element=Elementy.ID_Element GROUP BY Elementy.ID_Element
-) Stan INNER JOIN Elementy ON Stan.ID_Element=Elementy.ID_Element
+) Stan INNER JOIN Elementy ON Stan.ID_Element=Elementy.ID_Element INNER JOIN Elementy_Jednostki ON Elementy.ID_Jednostka=Elementy_Jednostki.ID_jednostka
 
 --towary na magazynie, którym koñczy siê termin przydatnoœci w ci¹gu najbli¿szych 2 miesiêcy
 
