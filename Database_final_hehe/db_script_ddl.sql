ca2e36b4b83a4b48704e219c87f381e2314e43e3
Magdalena Piotrowska <magdalena.piotrowska01@student.wat.edu.pl>
2020-05-04 10:12:19 +0200

    Uzupełniona tabela Oferta i kilka innych rekodrów dodanych do innych tabel

--- Database_final_hehe/db_script_ddl.sql
+++ Database_final_hehe/db_script_ddl.sql
@@ -512,3 +512,16 @@ FROM Elementy INNER JOIN
 	Elementy_Cechy_Slownik ON Elementy_Cechy.ID_Cecha = Elementy_Cechy_Slownik.ID_Cecha INNER JOIN 
 	Elementy_Jednostki ON Elementy_Cechy.ID_Jednostka = Elementy_Jednostki.ID_jednostka
 GO
+
+CREATE VIEW [dbo].[vRegaly]
+AS
+SELECT Regaly.Oznaczenie, Polki.ID_Polka, Elementy.Element_Nazwa, Zawartosc.Ilosc_Paczek, Elementy_Jednostki.Jednostka
+FROM            dbo.Elementy_Jednostki INNER JOIN
+    Elementy INNER JOIN
+    Oferta ON Elementy.ID_Element = Oferta.ID_Element ON Elementy_Jednostki.ID_jednostka = Oferta.ID_Jednostka INNER JOIN
+    Dostawy_Zawartosc ON dbo.Elementy.ID_Element = Dostawy_Zawartosc.ID_Element AND Oferta.ID_Oferta = Dostawy_Zawartosc.ID_oferta INNER JOIN
+    Zawartosc ON Elementy.ID_Element = Zawartosc.ID_Element INNER JOIN
+    Polki_regaly INNER JOIN
+    Polki ON dbo.Polki_regaly.ID_Polka = Polki.ID_Polka INNER JOIN
+    Regaly ON dbo.Polki_regaly.ID_Regal = Regaly.ID_Regal ON Zawartosc.ID_Polka = Polki.ID_Polka
+GO
